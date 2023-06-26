a05ed44475dc3acd39122dac02664952a6f7f66c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-06-26 21:25:25 +0300

    creating a note

--- js/script.js
+++ js/script.js
@@ -14,13 +14,37 @@ note.addEventListener('keydown', () => {
 saveButton.addEventListener('click', saveNote)
 
 function saveNote() {
+    document.querySelector('#popup').style.display = 'none'
+
     console.log('saved');
     localStorage.setItem(`${title.value}`, `${note.value}`)
 
+    document.querySelector('#savedNotes').innerHTML += `<h3>${title.value}</h3>` 
 }
 
 // displaying saved notes
 for (let i = 0; i < localStorage.length; i++) {
     const element = localStorage.key(i);
-    document.querySelector('#savedNotes').innerHTML += `<h3>${element}</h3>`    
+    document.querySelector('#savedNotes').innerHTML += `<h3 class="savedNote">${element}</h3>`    
 }    
+
+// creating a new note
+createNewButton.addEventListener('click', createNewNote)
+
+function createNewNote() {
+    document.querySelector('#popup').style.display = 'flex'
+
+    document.getElementById('currentSave').addEventListener('click', saveNote)
+
+    document.getElementById('continue').addEventListener('click', () => {
+        title.value = ''
+        note.value = ''
+
+        document.querySelector('span').innerText = 0
+
+        document.querySelector('#popup').style.display = 'none'
+    })
+
+}
+
+// editing a note
