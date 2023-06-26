7c124a75fc612191a8ca487d2b0dfb75ba6dff1a
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-06-26 19:30:15 +0300

    saving and displaying notes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+const title = document.querySelector('input')
 const note = document.querySelector('textarea')
+const saveButton = document.getElementById('save')
+const createNewButton = document.getElementById('new')
 
+// counting words
 note.addEventListener('keydown', () => {
     let text = note.value.split(' ')
 
-    if(!note.value){
     document.querySelector('span').innerText = text.length
-    }
 })
+
+// saving the notes
+saveButton.addEventListener('click', saveNote)
+
+function saveNote() {
+    console.log('saved');
+    localStorage.setItem(`${title.value}`, `${note.value}`)
+
+}
+
+// displaying saved notes
+for (let i = 0; i < localStorage.length; i++) {
+    const element = localStorage.key(i);
+    document.querySelector('#savedNotes').innerHTML += `<h3>${element}</h3>`    
+}    
